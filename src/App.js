93077055cdaf42f201980ea59f904dcd86f6cf38
Peter Sjögren <peter.sjogren@pskonsult.se>
2020-07-11 23:27:49 +0200

    Resize test

--- src/App.js
+++ src/App.js
@@ -6,20 +6,31 @@ import Area from "./Area";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { x: 0, y: 0 };
+    this.state = { x: 0, y: 0, width: 100, height: 100 };
   }
 
   render() {
     return (
       <>
         <Area
-          position={this.state}
+          position={{ x: this.state.x, y: this.state.y }}
+          width={this.state.width}
+          height={this.state.height}
           onDrag={(x, y) => this.setState({ x: x, y: y })}
+          onResize={(width, height) => {
+            console.log("height", height);
+            this.setState({ width: width, height: height });
+          }}
         ></Area>
         <br />
         <Area
           position={{ x: this.state.x / 2, y: this.state.y }}
+          width={this.state.width}
+          height={this.state.height}
           onDrag={(x, y) => this.setState({ x: x * 2, y: y })}
+          onResize={(width, height) =>
+            this.setState({ width: width, heigth: height })
+          }
         ></Area>
       </>
     );
--- src/Area.js
+++ src/Area.js
@@ -1,19 +1,53 @@
 import React from "react";
 import Draggable from "react-draggable";
+import { DraggableCore } from "react-draggable";
 
 export default function Area(props) {
   return (
-    <div style={{ width: "300px", height: "300px", background: "green" }}>
-      <Draggable
-        position={props.position}
-        onDrag={(e, d) => {
-          props.onDrag(d.x, d.y);
+    <>
+      <div
+        style={{
+          width: "300px",
+          height: "300px",
+          background: "green",
         }}
       >
-        <div
-          style={{ width: "50px", height: "50px", background: "blue" }}
-        ></div>
-      </Draggable>
-    </div>
+        <Draggable
+          position={{ x: props.position.x, y: props.position.y }}
+          onDrag={(e, d) => {
+            console.log("pos", d.x, d.y);
+            props.onDrag(d.x, d.y);
+          }}
+        >
+          <div
+            style={{
+              width: props.width + "px",
+              height: props.height + "px",
+              background: "blue",
+              position: "absolute",
+            }}
+          ></div>
+        </Draggable>
+        <Draggable
+          position={{
+            x: props.position.x + props.width,
+            y: props.position.y + props.height,
+          }}
+          onDrag={(e, d) => {
+            props.onResize(d.x - props.position.x, d.y - props.position.y);
+            console.log("pos2", props.position.y, d.y);
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              width: "20px",
+              height: "20px",
+              background: "red",
+            }}
+          ></div>
+        </Draggable>
+      </div>
+    </>
   );
 }
